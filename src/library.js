071b013da30a561112c8fc0d21c880a880501a2d
road2rhodes <132181574+road2rhodes@users.noreply.github.com>
2023-08-29 22:02:44 -0500

    Improve readability of book props when rendered

--- src/library.js
+++ src/library.js
@@ -13,26 +13,35 @@ const library = {
 
 class Book {
     constructor(title, author, pages, yearPublished, hasRead) {
-        this.bookInfo = () => {
-            const { title, author, pages, yearPublished, hasRead } = this;
-            return `Title: ${title},\nAuthor: ${author}\nPages: ${pages}\nYear published: ${yearPublished}\nRead yet? ${hasRead}`;
-        };
         this.title = title;
         this.author = author;
         this.pages = pages;
-        this.yearPublished = yearPublished;
-        this.hasRead = hasRead;
+        this["Year Published"] = yearPublished;
+        this["Read?"] = hasRead;
     }
     getReadStatus() {
         return this.hasRead;
     }
     ;
     setReadStatus(binaryNum) {
-        binaryNum === 1 ? this.hasRead = true : this.hasRead = false;
+        binaryNum === 1 ? this.hasRead = "yes" : this.hasRead = "no";
     }
 }
-const everyoneInThisRoom = new Book("Everyone in This Room Will Someday Be Dead", "Emily R. Austin", 256, 2021, false);
+const {title, author } = new Book("Everyone in This Room Will Someday Be Dead", "Emily R. Austin", 256, 2021, false);
+
+function addBookCard(title, author, pages,yearPublished, hasRead) {
+
+  let newBook = new Book(title, author, pages, yearPublished, hasRead);
+
+  for (const prop in newBook) {
+
+    let propName = prop[0].toUpperCase()+prop.slice(1)
+    let propValue = newBook[prop]
+
+    let temp = document.createElement("p")
+        temp.textContent = `${propName}: ${propValue}`;
+        document.body.appendChild(temp)
+  }
+}
+
 const addBooksDiv = document.querySelector(".add_books");
-addBooksDiv ? addBooksDiv.textContent = everyoneInThisRoom.bookInfo()
-    : console.log("No 'add_book's div found");
-//# sourceMappingURL=library.js.map
