b17818041713fbeb08b404d5d68cd1df1516805d
road2rhodes <132181574+road2rhodes@users.noreply.github.com>
2023-08-30 21:38:25 -0500

    Fix book deconstruction of string keys

--- src/library.js
+++ src/library.js
@@ -8,7 +8,8 @@ const library = {
   removefromLibrary: (book) => {
       let bookIndex = library.collection.indexOf(book);
       library.collection.splice(bookIndex, 1);
-  }
+  },
+  
 }
 
 class Book {
@@ -29,14 +30,21 @@ class Book {
 }
 
 const starterBooks = {
-  "Go Tell It On The Mountain": new Book("Go Tell It On the Mountain", "James Baldwin", 256, 1953, 1),
-  "The Guest List": new Book("The Guest List", "Lucy Foley", 319, 2020, 1),
-  "Ways of Seeing": new Book("Ways of Seeing", "John Berger", 176, 1972, 0)
+  "Go Tell It On The Mountain": new Book("Go Tell It On the Mountain", "James Baldwin", 256, 1953, "yes"),
+  "The Guest List": new Book("The Guest List", "Lucy Foley", 319, 2020, "no"),
+  "Ways of Seeing": new Book("Ways of Seeing", "John Berger", 176, 1972, "no")
 }
+
+const showBookCards = () => {library.collection.forEach(
+  ({title, author, pages, "Year Published": yearPublished, "Read?": hasRead}) => addBookCard(title, author, pages, yearPublished, hasRead)
+  )}
+
+  showBookCards();
 
 for (const bookTitle in starterBooks) {
   library.collection.push(starterBooks[bookTitle]);
 }
+
 const {title, author } = new Book("Everyone in This Room Will Someday Be Dead", "Emily R. Austin", 256, 2021, false);
 
 function addBookCard(title, author, pages,yearPublished, hasRead) {
